8f33e35c00b968c2ecccf407e06957d0746034ae
Dulustan <skyrockpk@gmail.com>
2023-03-07 13:55:12 +0700

    https://job4j.ru/profile/exercise/183/task-view/921 vacancy logic added

---
+++ src/main/java/com/dreamjob/controller/VacancyController.java
@@ -0,0 +1,22 @@
+package com.dreamjob.controller;
+
+import com.dreamjob.repository.MemoryVacancyRepository;
+import com.dreamjob.repository.VacancyRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacancies")
+public class VacancyController {
+
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("vacancies", vacancyRepository.findAll());
+        return "vacancies/list";
+    }
+
+}
---
+++ src/main/java/com/dreamjob/repository/MemoryVacancyRepository.java
@@ -0,0 +1,60 @@
+package com.dreamjob.repository;
+
+import com.dreamjob.model.Vacancy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    private MemoryVacancyRepository() {
+        save(new Vacancy(0, "Intern Java Developer", "New at this job", LocalDateTime.now()));
+        save(new Vacancy(0, "Junior Java Developer","Have experience about 1 year it this job", LocalDateTime.now()));
+        save(new Vacancy(0, "Junior+ Java Developer","More than 1 year of experience but have to improve some hard skills", LocalDateTime.now()));
+        save(new Vacancy(0, "Middle Java Developer","Have experience about 3 years and great hard and soft skills", LocalDateTime.now()));
+        save(new Vacancy(0, "Middle+ Java Developer","More than 3 years of experience but have to improve lead skills", LocalDateTime.now()));
+        save(new Vacancy(0, "Senior Java Developer","Have experience about 6 years have lead skills", LocalDateTime.now()));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return vacancies.remove(id) != null;
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+
+}
